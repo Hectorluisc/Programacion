6b9f94139bcf347f74316c5386f22427b85bfb39
Hector Luis <Hectorluisgolden@gmail.com>
2022-10-11 19:46:07 -0400

    Este es el ejemplo de la clase

--- Ejemplo 2.7.c
+++ Ejemplo 2.7.c
@@ -10,13 +10,25 @@ int main(){
 	if(a > b)
 	  if(a > c)
 	     if(b > c){
-	     	printf("El orden es a, b, c: %i %i %i",a, b, c);
-	     else
-		    printf("El orden es a, c, d: %i %i %i",a, c, d);
-		else 
-		    printf("El orden es c, a, b: %i %i %i",c, a, b);		
-		 }
+	     	printf("El orden es a, b, c: %i, %i, y %i",a, b, c);
+	    	      }
+	      else 
+	        printf("El orden es a, c, b: %i, %i y %i",a, c, b);
+	      
+		  else
+		    printf("El orden es c, a, b: %i, %i y %i",c, a, b);
 	
+	else 
+		if(b > c)
+		  if (a > c)
+		   printf("El orden es b, a, c: %i, %i y %i",b, a, c);
+		
+		else
+		   printf("El orden es b, c, a: %i, %i y %i",b, c, a);
+		
+		else
+		   printf("el orden es c, b, a %i, %i y %i",c, b, a);   
+		   	  
 	
 	
 	
