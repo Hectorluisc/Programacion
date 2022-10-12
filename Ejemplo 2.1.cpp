fe2890016d1c10a3c4594d34251a3389d9846b98
Hectorluisc <114534688+Hectorluisc@users.noreply.github.com>
2022-10-12 19:30:07 -0400

    Update Ejemplo 2.1.cpp

--- Ejemplo 2.1.cpp
+++ Ejemplo 2.1.cpp
@@ -3,11 +3,11 @@
 #include <stdio.h>
 int main (){
 	
-	int nota;
-	printf("Ingrese su promedio: ");
-	scanf("%i",&nota);
+	int nota;   //variable
+	printf("Ingrese su promedio: ");    //ingresar dato
+	scanf("%i",&nota);       //guardando ese dato
 	
-	if (nota>=6){
+	if (nota>=6){            //condicion de saber si se aprobo la materia
 		printf("Usted ha aprobado la materia.");
 	}
 
